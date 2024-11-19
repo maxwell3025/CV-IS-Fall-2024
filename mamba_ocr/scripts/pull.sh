034e7ed8abcbda161cf0cad15f506e4095b13a8c
maxwell3025 <amoinus@gmail.com>
2024-11-19 16:10:55 -0500

    Removed --ignore-missing-args

--- mamba_ocr/scripts/pull.sh
+++ mamba_ocr/scripts/pull.sh
@@ -8,8 +8,6 @@ source .env
 
 MODULE_NAME=$(basename $(pwd))
 
-rsync -rt unity:$UNITY_WORK_HOME/$MODULE_NAME/latest.log . \
-    --ignore-missing-args
+rsync -rt unity:$UNITY_WORK_HOME/$MODULE_NAME/latest.log .
 
-rsync -rt unity:$UNITY_WORK_HOME/$MODULE_NAME/output/ .output \
-    --ignore-missing-args
+rsync -rt unity:$UNITY_WORK_HOME/$MODULE_NAME/output/ .output
