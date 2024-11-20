b307cc832478f00f3c6c890482f23e89cc6778eb
maxwell3025 <amoinus@gmail.com>
2024-11-20 10:24:53 -0500

    Fixed install script

--- mamba_ocr/scripts/install.sh
+++ mamba_ocr/scripts/install.sh
@@ -8,6 +8,7 @@ test -e ../venv/bin/activate || exit 1
 
 source ../venv/bin/activate
 
+python -m pip install wheel packaging
 python -m pip install torch==2.4.0 torchaudio==2.4.0 torchvision==0.19.0 --index-url https://download.pytorch.org/whl/cpu
 MAMBA_SKIP_CUDA_BUILD=TRUE MAMBA_FORCE_BUILD=TRUE python -m pip install -e ../mamba-debugger --no-binary=mamba_ssm
 python -m pip install -e .[client]
