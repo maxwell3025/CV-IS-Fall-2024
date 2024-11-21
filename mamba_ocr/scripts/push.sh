2665f48f539534d23429e725d3460c47c5dff334
Donald Winkelman <dmwink.tx@gmail.com>
2024-11-21 13:38:45 -0500

    Small bug fixes and print statements

--- mamba_ocr/scripts/push.sh
+++ mamba_ocr/scripts/push.sh
@@ -8,7 +8,7 @@ source .env
 MODULE_NAME=$(basename $(pwd))
 
 rsync \
-    -R \
+    -rt \
     .env \
     $(git ls-files --others --exclude-standard --cached) \
     unity:$UNITY_WORK_HOME/$MODULE_NAME
--- mamba_ocr/src/mamba_ocr/train/__main__.py
+++ mamba_ocr/src/mamba_ocr/train/__main__.py
@@ -3,4 +3,5 @@ from src.models.resnet import resnet152
 from MedMamba.train import main
 
 if __name__ == '__main__':
+    print("this works")
     main()
