de45e486d1f8c9ba966aa7ffc288f7c652541530
maxwell3025 <amoinus@gmail.com>
2024-11-23 14:20:32 -0500

    Edited Dataloader Tests

--- mamba_ocr/src/mamba_ocr/data_loaders/mscoco_task_test.py
+++ mamba_ocr/src/mamba_ocr/data_loaders/mscoco_task_test.py
@@ -7,5 +7,5 @@ positional_encoding_vectors: numpy.ndarray = numpy.array([
     [1, 1],
 ])
 task = mscoco_task.MsCocoTask("./data/mscoco-text", positional_encoding_vectors, (1, 1))
-pyplot.matshow(task.features["train"][0][0].numpy().transpose())
+pyplot.matshow(task.features["train"][0][0].numpy().transpose()[:, -100:])
 pyplot.show()
