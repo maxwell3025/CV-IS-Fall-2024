04e76da654e8961d1155c093786ca179df7451fb
maxwell3025 <amoinus@gmail.com>
2024-12-08 22:13:15 -0500

    Added Figures and Text

---
+++ mamba_ocr/src/mamba_ocr/data_loaders/synthetic_text_task_test_letters.py
@@ -0,0 +1,33 @@
+from . import synthetic_text_task
+from matplotlib import pyplot
+import numpy
+positional_encoding_vectors = [
+    [1.0, 0.0],
+    [0.0, 1.0],
+    [1.0, 1.0],
+]
+task = synthetic_text_task.SyntheticTextTask(
+    100,
+    10,
+    positional_encoding_vectors,
+    32,
+    False,
+    False,
+    single_words=True,
+)
+for batch in task.batches(1):
+    [feature1, feature2, *_], [label1, label2, *_] = batch[0]
+
+    pyplot.imshow(feature1.permute(1, 2, 0))
+    label1 = label1.argmax(dim=1)
+    label1 = label1.tolist()
+    label1 = [task.get_index_alphabet(x) for x in label1]
+    print(label1)
+    pyplot.show()
+
+    pyplot.imshow(feature2.permute(1, 2, 0))
+    label2 = label2.argmax(dim=1)
+    label2 = label2.tolist()
+    label2 = [task.get_index_alphabet(x) for x in label2]
+    print(label2)
+    pyplot.show()
